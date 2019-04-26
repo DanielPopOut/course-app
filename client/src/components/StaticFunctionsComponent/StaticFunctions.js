fe291614deb309a4f1084b331ed851c519b4c2c7
pakyt <cyrilledassie@gmail.com>
2019-04-26 16:26:38 +0100

    CD. restoring function for email control

--- client/src/components/StaticFunctionsComponent/StaticFunctions.js
+++ client/src/components/StaticFunctionsComponent/StaticFunctions.js
@@ -1,10 +1,10 @@
 export const validateEmail = function (email) {
-    var re = /^(([^<>()[\]\\.,;:s@"]+(\.[^<>()[\]\\.,;:s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 };
 
 export const validatePhoneNumber = function (phonenumber) {
-    var phoneno = /^d{9}$/;
+    var phoneno = /^\d{9}$/;
     if ((phonenumber.match(phoneno)))
     {
         return true;
@@ -14,17 +14,30 @@ export const validatePhoneNumber = function (phonenumber) {
     }
 };
 
+export const loadDataIntoModel=(datamodel,data)=>{
+    let fields=[];
+    console.log(datamodel);
+    datamodel.fields.forEach((row,index)=>{
+        fields.push(Object.assign({},row,{value:data[row.name]}));
+    });
+    datamodel=Object.assign({},datamodel,{fields:fields});
+    console.log(datamodel);
+    return datamodel;
+};
 
 export const validatePassword = (password)=>{
     console.log("password sended ",password);
-    var passw=  /^[A-Za-z]w{6,14}$/;
+    var passw=  /^[A-Za-z]\w{6,14}$/;
     if(password.match(passw))
     {
+        //alert('Correct, try another...')
         return true;
     }
     else
     {
+        //alert('Wrong...!')
         return false;
     }
 
+    return true;
 };
